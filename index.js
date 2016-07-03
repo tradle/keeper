0075a86798926d60d3fdbb01ac9de00cce9344a6
Mark Vayngrib <mark@tradle.io>
2016-07-03 11:27:36 -0400

    use hex keys

--- index.js
+++ index.js
@@ -99,5 +99,5 @@ module.exports = function keeper (opts) {
 // }
 
 function sha256 (data) {
-  return crypto.createHash('sha256').update(data).digest()
+  return crypto.createHash('sha256').update(data).digest('hex')
 }
